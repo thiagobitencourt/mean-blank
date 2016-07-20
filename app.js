0050261e2368dac720d0b89a1421f32b5d866c07
Thiago R. M. Bitencourt <thiago.mbitencourt@gmail.com>
2016-07-20 00:02:32 -0300

    Add: package.json and node minimun app

--- app.js
+++ app.js
@@ -2,5 +2,13 @@
   @Author: Thiago R. M. Bitencourt
   Node.js app entry
 */
+var express = require('express');
+var http = require('http');
 
-console.log("We are up!");
+var app = express();
+app.use('/', express.static('public/src'));
+
+var httpPort = 8080;
+http.createServer(app).listen(httpPort, function(){
+  console.log("HTTP server listening on port %s", httpPort);
+});
